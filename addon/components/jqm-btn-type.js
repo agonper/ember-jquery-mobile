8b5f7920a596b381a00265c8d02dd86208eebd81
albertogonper <alberto.gonzalez95@gmail.com>
2015-10-26 21:44:29 +0100

    Typo correction

--- addon/components/jqm-btn-type.js
+++ addon/components/jqm-btn-type.js
@@ -1,4 +1,4 @@
-import JqmComponent from './jqm-component'
+import JqmComponent from './jqm-component';
 import layout from '../templates/components/jqm-btn-type';
 
 export default JqmComponent.extend({
