ad0f0bf18090e077f5c49087b8326bb14e3ae6bf
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-04 19:49:25 +0100

    Now jqm-select triggers onChange event

--- addon/components/jqm-select.js
+++ addon/components/jqm-select.js
@@ -1,8 +1,12 @@
 import layout from '../templates/components/jqm-select';
+import $ from 'jquery';
 import JqmComponent from './jqm-component';
 
 export default JqmComponent.extend({
   layout: layout,
   tagName: 'select',
-  attributeBindings: ['mini:data-mini', 'iconPos:data-iconpos']
+  attributeBindings: ['mini:data-mini', 'iconPos:data-iconpos'],
+  change() {
+    this.sendAction('action', $(this.$()).val());
+  }
 });
