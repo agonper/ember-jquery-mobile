e03cb7e7580218a226ffa758d312edb621c56e65
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-10 17:57:55 +0100

    Slider now accepts theme property

--- addon/components/jqm-slider.js
+++ addon/components/jqm-slider.js
@@ -4,7 +4,7 @@ import layout from '../templates/components/jqm-slider';
 
 export default Ember.TextField.extend({
   layout: layout,
-  attributeBindings: ['mini:data-mini', 'highlight:data-highlight', 'dataType:data-type',
+  attributeBindings: ['mini:data-mini', 'highlight:data-highlight', 'dataType:data-type', 'theme:data-theme',
     'autofocus', 'name', 'required', 'autocomplete'],
   type: "number",
   dataType: "range",
