b0396a3afd56453133d8a146a55a20325b33ce24
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-10 17:27:38 +0100

    Textarea is now form ready (attr: autofocus, name, required)

--- addon/components/jqm-textarea.js
+++ addon/components/jqm-textarea.js
@@ -3,5 +3,6 @@ import layout from '../templates/components/jqm-textarea';
 
 export default Ember.TextArea.extend({
   layout: layout,
-  attributeBindings: ['clear-btn:data-clear-btn', 'mini:data-mini']
+  attributeBindings: ['clear-btn:data-clear-btn', 'mini:data-mini',
+    'autofocus', 'name', 'required']
 });
