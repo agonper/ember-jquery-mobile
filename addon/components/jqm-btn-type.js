fe2e6f725af2a98821f3811ca0159ace73093f5b
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-10 17:21:10 +0100

    Buttons and anchors now can be used correctly on forms

--- addon/components/jqm-btn-type.js
+++ addon/components/jqm-btn-type.js
@@ -3,7 +3,8 @@ import layout from '../templates/components/jqm-btn-type';
 
 export default JqmComponent.extend({
   layout: layout,
-  attributeBindings: ['icon:data-icon', 'iconPos:data-iconpos', 'mini:data-mini', 'inline:data-inline', 'title'],
+  attributeBindings: ['icon:data-icon', 'iconPos:data-iconpos', 'mini:data-mini', 'inline:data-inline',
+    'title', 'autofocus', 'name', 'required'],
   click() {
     this.sendAction();
   }
