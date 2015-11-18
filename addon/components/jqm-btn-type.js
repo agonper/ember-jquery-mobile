9433d6064af0dd692572dd1757199606dc604539
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-18 15:25:33 +0100

    Buttons & Anchors now can be hidden an shown. prop: isShown

--- addon/components/jqm-btn-type.js
+++ addon/components/jqm-btn-type.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+import Ember from 'ember';
 import JqmComponent from './jqm-component';
 import layout from '../templates/components/jqm-btn-type';
 
@@ -5,6 +7,16 @@ export default JqmComponent.extend({
   layout: layout,
   attributeBindings: ['icon:data-icon', 'iconPos:data-iconpos', 'mini:data-mini', 'inline:data-inline',
     'title', 'autofocus', 'name', 'required'],
+  isShown: true,
+  showStatus: Ember.on('didInsertElement',
+    Ember.observer('isShown', function () {
+      if (this.get('isShown')) {
+        $(this.$()).show();
+      } else {
+        $(this.$()).hide();
+      }
+    }
+  )),
   click() {
     this.sendAction();
   }
