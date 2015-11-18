bba410dfc8b2cb2e9a0b15841e074449bf4f5724
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-18 15:34:58 +0100

    Now all components can be hidden. isShown=false

--- addon/components/jqm-btn-type.js
+++ addon/components/jqm-btn-type.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-import Ember from 'ember';
 import JqmComponent from './jqm-component';
 import layout from '../templates/components/jqm-btn-type';
 
@@ -7,16 +5,6 @@ export default JqmComponent.extend({
   layout: layout,
   attributeBindings: ['icon:data-icon', 'iconPos:data-iconpos', 'mini:data-mini', 'inline:data-inline',
     'title', 'autofocus', 'name', 'required'],
-  isShown: true,
-  showStatus: Ember.on('didInsertElement',
-    Ember.observer('isShown', function () {
-      if (this.get('isShown')) {
-        $(this.$()).show();
-      } else {
-        $(this.$()).hide();
-      }
-    }
-  )),
   click() {
     this.sendAction();
   }
--- addon/components/jqm-component.js
+++ addon/components/jqm-component.js
@@ -6,6 +6,16 @@ export default Ember.Component.extend({
   layout: layout,
   attributeBindings: ['role:data-role', 'theme:data-theme', 'corners:data-corners', 'shadow:data-shadow'],
   classNameBindings: ['allCorners:ui-corner-all', 'overlayShadow:ui-overlay-shadow'],
+  isShown: true,
+  showStatus: Ember.on('didInsertElement',
+    Ember.observer('isShown', function () {
+        if (this.get('isShown')) {
+          $(this.$()).show();
+        } else {
+          $(this.$()).hide();
+        }
+      }
+    )),
   didInsertElement() {
     $(this.$()).enhanceWithin();
   }
