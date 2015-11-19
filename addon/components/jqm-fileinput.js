05429df9cbb80df6b10693a5c1af8b5ff0dc0014
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-19 16:31:07 +0100

    FileInput management at a lower level. onFile(evt)

--- addon/components/jqm-fileinput.js
+++ addon/components/jqm-fileinput.js
@@ -4,28 +4,12 @@ import layout from '../templates/components/jqm-fileinput';
 export default JqmTextInput.extend({
   layout: layout,
   type: 'file',
-  file: null,
-  uploading: false,
-  onLoad: null, // Callback function <Action>
+  //file: null,
+  //uploading: false,
+  onFile: null, // Callback function <Action>
   change(evt) {
-    var file = evt.target.files[0];
-
-    if (file && !this.get('uploading')) {
-      this.set('uploading', true);
-      var reader = new FileReader();
-      reader.onload = () => {
-        this.set('file', reader.result);
-        let onLoad = this.get('onLoad');
-        if (onLoad) {
-          this.sendAction('onLoad');
-        }
-      };
-      reader.onloadend = () => {
-        this.set('uploading', false);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      this.set('file', null);
+    if (this.get('onFile')) {
+      this.sendAction('onFile', evt);
     }
   }
 });
