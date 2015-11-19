48217dff477372a1514bfd5895ca15cb5b3256ea
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-19 02:51:07 +0100

    FileInput tested

--- addon/components/jqm-fileinput.js
+++ addon/components/jqm-fileinput.js
@@ -3,5 +3,29 @@ import layout from '../templates/components/jqm-fileinput';
 
 export default JqmTextInput.extend({
   layout: layout,
-  type: 'file'
+  type: 'file',
+  file: null,
+  uploading: false,
+  onLoad: null, // Callback function <Action>
+  change(evt) {
+    var file = evt.target.files[0];
+
+    if (file && !this.get('uploading')) {
+      this.set('uploading', true);
+      var reader = new FileReader();
+      reader.onload = () => {
+        this.set('file', reader.result);
+        let onLoad = this.get('onLoad');
+        if (onLoad) {
+          this.sendAction('onLoad');
+        }
+      };
+      reader.onloadend = () => {
+        this.set('uploading', false);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      this.set('file', null);
+    }
+  }
 });
