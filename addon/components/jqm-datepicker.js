60a438807c9600a0557bf65ade54ea693432530f
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-20 22:07:19 +0100

    Now date is correctly retrived form datepicker value

--- addon/components/jqm-datepicker.js
+++ addon/components/jqm-datepicker.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 import layout from '../templates/components/jqm-datepicker';
 
 export default Ember.TextField.extend({
+  tagName: 'input',
   layout: layout,
   readonly: 'readonly',
   attributeBindings: ['required', 'name', 'autofocus'],
@@ -10,15 +11,23 @@ export default Ember.TextField.extend({
   showToday: false,
   didInsertElement() {
     var elem = $(this.$());
+    var that = this;
     elem.datepicker({
-      dateFormat: this.get('dateFormat')
+      dateFormat: this.get('dateFormat'),
+      onSelect: function () {
+        Ember.run.once(that, 'refresh');
+      }
     });
 
     if (this.get('showToday')) {
       elem.datepicker('setDate', new Date());
+      this.$().change();
     }
 
     // Remove datepicker from the bottom of the page
     $( '.ui-datepicker' ).css('display', 'none');
+  },
+  refresh() {
+    this.$().change();
   }
 });
