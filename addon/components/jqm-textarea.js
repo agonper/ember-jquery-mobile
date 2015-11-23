1575a816d36887b746576b525d137b7f145de2d5
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-23 21:21:33 +0100

    jqm-textinput & jqm-textarea now can be dynamically injected

--- addon/components/jqm-textarea.js
+++ addon/components/jqm-textarea.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
+import $ from 'jquery';
 import layout from '../templates/components/jqm-textarea';
 
 export default Ember.TextArea.extend({
   layout: layout,
   attributeBindings: ['clear-btn:data-clear-btn', 'mini:data-mini',
-    'autofocus', 'name', 'required']
+    'autofocus', 'name', 'required'],
+  didInsertElement() {
+    $(this.$()).textinput();
+  }
 });
--- addon/components/jqm-textinput.js
+++ addon/components/jqm-textinput.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
+import $ from 'jquery';
 import layout from '../templates/components/jqm-textinput';
 
 export default Ember.TextField.extend({
   layout: layout,
   attributeBindings: ['clearBtn:data-clear-btn', 'mini:data-mini', 'autofocus',
-    'name', 'required', 'autocomplete']
+    'name', 'required', 'autocomplete'],
+  didInsertElement() {
+    $(this.$()).textinput();
+  }
 });
