6433542ae48d65bb61bef8599af63193e9438f53
albertogonper <alberto.gonzalez95@gmail.com>
2016-04-13 21:02:25 +0200

    Some extra functionality for checkboxes & radiobuttons

--- addon/components/jqm-checkbox.js
+++ addon/components/jqm-checkbox.js
@@ -5,7 +5,7 @@ import layout from '../templates/components/jqm-checkbox';
 export default Ember.Checkbox.extend({
   layout: layout,
   attributeBindings: ['clearBtn:data-clear-btn', 'mini:data-mini', 'autofocus',
-    'name', 'required', 'autocomplete'],
+    'name', 'required', 'autocomplete', 'theme:data-theme', 'enhanced:data-enhanced'],
   didInsertElement() {
     var checkbox = $(this.$());
     var _this = this;
@@ -14,5 +14,14 @@ export default Ember.Checkbox.extend({
     checkbox.on('change', () => {
       _this.set('checked', checkbox.prop('checked'));
     });
-  }
+  },
+
+  checkChanged: Ember.on('didInsertElement', Ember.observer('checked', function () {
+    var checkbox = this.get('elem');
+    if (checkbox) {
+      var checked = this.get('checked');
+      checkbox.prop('checked', checked);
+      checkbox.checkboxradio('refresh');
+    }
+  }))
 });
